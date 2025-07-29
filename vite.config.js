e9de9d380a3369c359872658131ef3b3cac95020
haysie09 <heyitshaysie@gmail.com>
2025-07-29 21:36:13 +1200

    bug: tail

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from 'tailwindcss' // Import tailwindcss
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
+  // This is the new part we are adding
+  css: {
+    postcss: {
+      plugins: [tailwindcss()],
+    },
+  },
   plugins: [react()],
 })
